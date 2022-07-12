ead6330131718cbf02ced3b549cde6cef2ee2132
Max Turer <turerhal@gmail.com>
2022-07-11 23:44:30 -0400

    template setup

---
+++ src/components/Hello.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+    name: string
+}>()
+
+const uppercase = computed(() => props.name.toLocaleUpperCase());
+</script>
+
+<template>
+  <div text="red-400 3xl" font="serif bold">Hello, {{ uppercase }}</div>
+  <div i-teenyicons:globe-americas-solid text-5xl text-blue-300></div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import { presetAttributify, presetUno, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [
+    Vue(),
+    Components(),
+    AutoImport({
+      imports: [
+        'vue'
+      ]
+    }),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
+      ]
+    }),
+  ]
 })
+
